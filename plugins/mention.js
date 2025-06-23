2b2b85a8b40eec6cd629483a1beb7664e3893e5c
Rizwan-dev79 <samhaxhosts@gmail.com>
2025-06-23 05:21:10 +0500

    Update mention.js

--- plugins/mention.js
+++ plugins/mention.js
@@ -1,4 +1,7 @@
-const { bot, mentionMessage, enableMention, clearFiles, getMention, lang } = require('../lib/')
+const { cmd } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+const config = require('../config');
 
 bot(
   {
@@ -7,27 +10,43 @@ bot(
     type: 'misc',
   },
   async (message, match) => {
-    if (!match) {
-      const mention = await getMention(message.id)
-      const onOrOff = mention && mention.enabled ? 'on' : 'off'
-      return await message.send(lang.plugins.mention.current_status.format(onOrOff))
-    }
+    try {
+      // Handle case when no match is provided (show current status)
+      if (!match) {
+        const mention = await getMention(message.id);
+        const status = mention?.enabled ? 'on' : 'off';
+        return await message.send(
+          lang.plugins.mention.current_status.format(status)
+        );
+      }
 
-    if (match === 'get') {
-      const msg = await mentionMessage(message.id)
-      if (!msg) return await message.send(lang.plugins.mention.not_activated)
-      return await message.send(msg)
-    }
+      // Handle 'get' command
+      if (match === 'get') {
+        const msg = await mentionMessage(message.id);
+        if (!msg) {
+          return await message.send(lang.plugins.mention.not_activated);
+        }
+        return await message.send(msg);
+      }
 
-    if (match === 'on' || match === 'off') {
-      await enableMention(match === 'on', message.id)
-      return await message.send(
-        match === 'on' ? lang.plugins.mention.activated : lang.plugins.mention.deactivated
-      )
-    }
+      // Handle enable/disable commands
+      if (match === 'on' || match === 'off') {
+        await enableMention(match === 'on', message.id);
+        const response = match === 'on' 
+          ? lang.plugins.mention.activated 
+          : lang.plugins.mention.deactivated;
+        return await message.send(response);
+      }
 
-    await enableMention(match, message.id)
-    clearFiles()
-    return await message.send(lang.plugins.mention.updated)
+      // Handle custom mention message
+      await enableMention(match, message.id);
+      clearFiles();
+      return await message.send(lang.plugins.mention.updated);
+      
+    } catch (error) {
+      console.error('Error in mention plugin:', error);
+      // Optionally send an error message to the user
+      // return await message.send(lang.plugins.mention.error);
+    }
   }
-)
+);
